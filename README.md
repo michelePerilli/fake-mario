d8d17b3fe4958c7b762c0e9b73b67951808113fc
Michele <micheleperilli.98@libero.it>
2022-03-19 21:16:15 +0100

    project created

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# fake-mario
+# Fake Mario
+Just for learning purpose: coding a 2D game 
---
+++ src/main/java/Launcher.java
@@ -0,0 +1,8 @@
+/**
+ * Project: fake-mario
+ * Author: Michele
+ * File: Launcher
+ * Creation: 19/03/2022
+ */
+public class Launcher {
+}
